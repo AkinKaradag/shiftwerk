042c125593a08180ea9162148b12265cb3553076
Akin Karadag <akin.karadag@outlook.com>
2026-08-04 20:25:54 +0200

    Initialize Express application with basic structure and routes

---
+++ src/routes/users.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('users', {
+    title: 'Shiftwerk Users',
+    user: { name: 'John Doe' },
+    users: [
+      { name: 'John Doe' },
+      { name: 'Jane Doe' },
+      { name: 'Jim Doe' }
+    ]
+  });
+});
+
+module.exports = router;
